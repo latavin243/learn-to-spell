4db0b9761e4b55e11d349ce66e364a8def5b75dd
guoqi <qi_guo@shannonai.com>
2020-03-03 01:04:54 +0800

    240 improved

--- 240.cpp
+++ 240.cpp
@@ -7,34 +7,11 @@ public:
          */
         if(matrix.size() == 0 || matrix[0].size() == 0) return false;
 
-        int curCol = 0, preCol = 0, colLen = matrix[0].size();
-        for (int i = 0; i < matrix.size(); i++) { // row
-            bool fromSmall = false, fromLarge = false;
-            while (matrix[i][curCol] < target) {
-                if(curCol + 1 < colLen) {
-                    fromSmall = true;
-                    curCol++;
-                }
-                else break;
-            }
-            if(matrix[i][curCol] == target) return true;
-            if(fromSmall) {
-                curCol = min(0, curCol-1);
-                continue;
-            }
-
-            while (matrix[i][curCol] > target) {
-                if(curCol - 1 >= 0) {
-                    fromLarge = true;
-                    curCol--;
-                }
-                else break;
-            }
-            if(matrix[i][curCol] == target) return true;
-            if(fromLarge) {
-                curCol = max(curCol+1, colLen-1);
-                continue;
-            }
+        int n = matrix.size(), m = matrix[0].size(), i = 0, j = m-1;
+        while(i < n && j >= 0) {
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) j--;
+            else i++;
         }
         return false;
     }
