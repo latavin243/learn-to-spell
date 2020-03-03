f26436ee6a579142dbf584b433c67f36aca0796d
guoqi <qi_guo@shannonai.com>
2020-03-03 12:02:46 +0800

    887 improved

--- 887.cpp
+++ 887.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     int superEggDrop(int K, int N) {
-        vector<vector<int>> dp(K+1, vector<int> (N+1, 0));
-
-        // one egg, K=1
-        for (int j = 1; j <= N; j++)
-            dp[1][j] = j;
-
-        for (int i = 2; i <= K; i++) {
-            for (int j = 1; j <= N; j++) {
-                int tmp = j;
-                for (int k = 1; k < j; k++) {
-                    int worse = max(dp[i-1][k-1], dp[i][j-k]) + 1;
-                    tmp = min(worse, tmp);
-                }
-                dp[i][j] = tmp;
-            }
+        vector<vector<int>> dp(N+1, vector<int> (K+1, 0));
+        // m: times of drop, k: egg number, dp[m][k]: floors can reach
+        int m = 0;
+        while(dp[m][K] < N) {
+            m++;
+            for (int k = 1; k <= K; k++)
+                dp[m][k] = dp[m-1][k] + dp[m-1][k-1] + 1;
         }
-        return dp[K][N];
+        return m;
     }
 };
