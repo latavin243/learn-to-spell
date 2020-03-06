6308a1902a1973bd682109def1b99dd92a5d6281
Qi Guo <guoqi2014@sjtu.edu.cn>
2020-03-06 21:07:07 +0800

    169 improved

--- 169.cpp
+++ 169.cpp
@@ -1,12 +1,32 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         // non-empty, must exist, more than n/2 times
+//         unordered_map<int, int> m;
+//         for (int n : nums) {
+//             m[n]++;
+//             if (m[n] > nums.size() / 2) return n;
+//         }
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         // non-empty, must exist, more than n/2 times
-        unordered_map<int, int> m;
+        int curNum = 0, count = 0;
         for (int n : nums) {
-            m[n]++;
-            if (m[n] > nums.size() / 2) return n;
+            if (count == 0) {
+                curNum = n;
+                count = 1;
+                continue;
+            }
+
+            if (n == curNum) count++;
+            else count--;
+
         }
-        return -1;
+        return curNum;
     }
 };
